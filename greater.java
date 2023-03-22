74e97a222a24045f4118381fa35b6009e42f90a4
06swetha <113409283+06swetha@users.noreply.github.com>
2023-03-22 14:03:55 +0530

    Add files via upload

---
+++ greater.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+class greater
+{
+public static void main(String arg[])
+{
+int a,b;
+Scanner sc=new Scanner (System.in);
+a=sc.nextInt();
+b=sc.nextInt();
+if(a>b)
+{
+System.out.println("greater number is"+a);
+}
+else 
+{
+System.out.println("greater number is"+b);
+}
+}
+};
